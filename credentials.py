09baddd74bf38b1683c0e23af0ff4cde0147ac1b
LeeXiangZe <128678585+LeeXiangZe@users.noreply.github.com>
2023-03-23 15:20:30 +0800

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS LIBRARY 
+    (ID INT PRIMARY KEY   NOT NULL,
+    TITLE        TEXT   NOT NULL,
+    AUTHOR       TEXT   NOT NULL,
+    PUBLISHER    TEXT   NOT NULL);''')
+
+conn.execute("INSERT INTO LIBRARY (ID,TITLE,AUTHOR,PUBLISHER) \
+    VALUES (1, 'Test', 'California', 'A1 Pic' )");
+
+
+cursor = conn.execute("SELECT ID, title, author, publisher from LIBRARY")
+for row in cursor:
+    print(row)
+
+conn.commit()
+conn.close()
