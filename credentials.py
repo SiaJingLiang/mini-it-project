fc71be5a8a86fa379f1aa53ad0349f1d6428cf18
Foreen03 <128678869+Foreen03@users.noreply.github.com>
2023-03-29 09:49:32 +0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -52,6 +52,39 @@ def studentFeature():
 def managerFeature():
     print("managerFeature")
 
+def book_func():
+    
+    c.execute('''CREATE TABLE IF NOT EXISTS TEST 
+                (BOOK_ID INT PRIMARY KEY NOT NULL, 
+                BOOK_NAME TEXT NOT NULL, 
+                BOOK_AMOUNT INT NOT NULL, 
+                BOOK_STATUS TEXT NOT NULL)''')
+        
+    book_id = int(input('Enter Book ID: '))
+    fetch  = c.execute('SELECT BOOK_ID from TEST WHERE BOOK_ID=?', (book_id,))
+    while book_id == '':
+        print('Book_ID cannot be empty.')
+        book_id = int(input('Enter Book ID: '))
+    while book_id == '' or fetch.fetchone() != None:
+        print('ID has already excisted.')
+        book_id = int(input('Enter Book ID: '))
+
+    book_name = str(input('Enter Book Name: '))
+    while book_name == '':
+        print('Book Name cannot be empty.')
+        book_name = str(input('Enter Book Name: '))
+    
+    book_amount = int(input('Enter Book Amount: '))
+    while book_amount == '':
+        print('Please enter an value.')
+        book_amount = int(input('Enter Book Amount: '))
+    
+    book_status = str(input('Enter book status(T/F): '))
+    
+    c.execute('INSERT INTO TEST (BOOK_ID,BOOK_NAME,BOOK_AMOUNT,BOOK_STATUS) VALUES(?,?,?,?)',(book_id,book_name,book_amount,book_status))
+    print('Added succesfully.')
+    conn.commit()    
+
 def menu():
     login = False
     choice = int(input("[1]Login \n[2]Sign Up \n[3]End \nEnter your choice: "))
